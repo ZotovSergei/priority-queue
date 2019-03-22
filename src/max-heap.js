d23c501ddab4266f986717c1d665b1d91c10b25a
Зотов Сергей Валерьевич <__@m-invest.local>
2019-03-22 14:47:52 +0500

    feature(priority-queue): *first realize data*

--- src/max-heap.js
+++ src/max-heap.js
@@ -1,16 +1,26 @@
 const Node = require('./node');
-
+let heap = null;
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+			const node = new Node(data,priority);
+			this.insertNode(node);
+			this.shiftNodeUp(node);
 	}
 
 	pop() {
-		
+		if (heap == null) {}
+		this.root = 3;
+		let data = this.root;
+		heap = this.root
+		let detachRoot =  this.detachRoot();
+		this.restoreRootFromLastInsertedNode(detachRoot);
+		this.shiftNodeDown(heap);
+		return data;
 	}
 
 	detachRoot() {
@@ -34,7 +44,9 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
-		
+		if (heap == null) {
+			
+		}
 	}
 
 	shiftNodeUp(node) {
--- src/node.js
+++ src/node.js
@@ -1,22 +1,34 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
 
+		if (this.left && this.right) { }
+		else if (this.left) { this.right = node; }
+		else if (!this.left) { this.left = node; }
 	}
 
 	removeChild(node) {
-
+		if (node == this.left) { this.left = null; }
+		else if (node == this.right) { this.right = null; }
+		else { throw "error" }
 	}
 
 	remove() {
-
+		if (this.parent == null) { }
+		else {
+			this.parent.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		this.parent.parent = this.parent;
 	}
 }
 
